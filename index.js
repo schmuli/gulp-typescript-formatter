4228a3233d9516f2d487ecb6fa81f7a39e1558c0
Schmulik Raskin <schmuli@gmail.com>
2017-02-27 21:02:50 +0200

    Update to latest versions

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-"use strict";
+"use strict"
 
 var PluginError = require('gulp-util').PluginError;
 var through = require('through2');
 var formatter = require('typescript-formatter');
 
 function gulpTypescriptFormatter(options) {
-  return through.obj(function(file, enc, cb) {
-    if (file.isNull()) {
-      // return empty file
-      return cb(null, file);
-    }
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            // return empty file
+            return cb(null, file);
+        }
 
-    if (file.isBuffer()) {
-      var fileContentPromise = formatter.processString(file.path, String(file.contents), options);
+        if (file.isBuffer()) {
+            var fileContentPromise = formatter.processString(file.path, String(file.contents), options);
 
-      fileContentPromise.then(function(result) {
-        file.contents = new Buffer(result.dest);
+            fileContentPromise.then(function (result) {
+                file.contents = new Buffer(result.dest);
 
-        cb(null, file);
-      });
-    }
+                cb(null, file);
+            });
+        }
 
-    if (file.isStream()) {
-      return cb(new PluginError('gulp-typescript-formatter', 'Streaming not supported'));
-    }
-  });
-
+        if (file.isStream()) {
+            return cb(new PluginError('gulp-typescript-formatter', 'Streaming not supported'));
+        }
+    });
 }
 
 module.exports = gulpTypescriptFormatter;
